ca972834ba22d620bce006eee5abc8b3be24130b
paladin4ua <igzhva@gmail.com>
2018-07-28 23:28:54 +0300

    preparations and fixes for BsaHw10

--- Airport.Data/Migrations/20180715082908_InitialCreate.cs
+++ Airport.Data/Migrations/20180715082908_InitialCreate.cs
@@ -4,225 +4,225 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Airport.Data.Migrations
 {
-    public partial class InitialCreate : Migration
+  public partial class InitialCreate : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Crew",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    PilotId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Crew", x => x.Id);
-                });
+      migrationBuilder.CreateTable(
+          name: "Crew",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            PilotId = table.Column<int>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Crew", x => x.Id);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Flight",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Number = table.Column<string>(nullable: true),
-                    DeparturePlace = table.Column<string>(nullable: true),
-                    DepartureTime = table.Column<DateTime>(nullable: false),
-                    ArrivalPlace = table.Column<string>(nullable: true),
-                    ArrivalTime = table.Column<DateTime>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Flight", x => x.Id);
-                });
+      migrationBuilder.CreateTable(
+          name: "Flight",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            Number = table.Column<string>(nullable: true),
+            DeparturePlace = table.Column<string>(nullable: true),
+            DepartureTime = table.Column<DateTime>(nullable: false),
+            ArrivalPlace = table.Column<string>(nullable: true),
+            ArrivalTime = table.Column<DateTime>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Flight", x => x.Id);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Pilot",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    FirstName = table.Column<string>(nullable: true),
-                    LastName = table.Column<string>(nullable: true),
-                    BirthDate = table.Column<DateTime>(nullable: false),
-                    Experience = table.Column<double>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Pilot", x => x.Id);
-                });
+      migrationBuilder.CreateTable(
+          name: "Pilot",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            FirstName = table.Column<string>(nullable: true),
+            LastName = table.Column<string>(nullable: true),
+            BirthDate = table.Column<DateTime>(nullable: false),
+            Experience = table.Column<double>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Pilot", x => x.Id);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "PlaneType",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Model = table.Column<string>(nullable: true),
-                    Seats = table.Column<int>(nullable: false),
-                    Carrying = table.Column<double>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PlaneType", x => x.Id);
-                });
+      migrationBuilder.CreateTable(
+          name: "PlaneType",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            Model = table.Column<string>(nullable: true),
+            Seats = table.Column<int>(nullable: false),
+            Carrying = table.Column<double>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_PlaneType", x => x.Id);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Airhostess",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    FirstName = table.Column<string>(nullable: true),
-                    LastName = table.Column<string>(nullable: true),
-                    BirthDate = table.Column<DateTime>(nullable: false),
-                    CrewId = table.Column<int>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Airhostess", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Airhostess_Crew_CrewId",
-                        column: x => x.CrewId,
-                        principalTable: "Crew",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
+      migrationBuilder.CreateTable(
+          name: "Airhostess",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            FirstName = table.Column<string>(nullable: true),
+            LastName = table.Column<string>(nullable: true),
+            BirthDate = table.Column<DateTime>(nullable: false),
+            CrewId = table.Column<int>(nullable: true)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Airhostess", x => x.Id);
+            table.ForeignKey(
+                      name: "FK_Airhostess_Crew_CrewId",
+                      column: x => x.CrewId,
+                      principalTable: "Crew",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.SetNull);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Ticket",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Price = table.Column<double>(nullable: false),
-                    FlightId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Ticket", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Ticket_Flight_FlightId",
-                        column: x => x.FlightId,
-                        principalTable: "Flight",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+      migrationBuilder.CreateTable(
+          name: "Ticket",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            Price = table.Column<double>(nullable: false),
+            FlightId = table.Column<int>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Ticket", x => x.Id);
+            table.ForeignKey(
+                      name: "FK_Ticket_Flight_FlightId",
+                      column: x => x.FlightId,
+                      principalTable: "Flight",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.Cascade);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Plane",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    PlaneTypeId = table.Column<int>(nullable: false),
-                    ReleaseDate = table.Column<DateTime>(nullable: false),
-                    ServiceLife = table.Column<TimeSpan>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Plane", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Plane_PlaneType_PlaneTypeId",
-                        column: x => x.PlaneTypeId,
-                        principalTable: "PlaneType",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+      migrationBuilder.CreateTable(
+          name: "Plane",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            Name = table.Column<string>(nullable: true),
+            PlaneTypeId = table.Column<int>(nullable: false),
+            ReleaseDate = table.Column<DateTime>(nullable: false),
+            ServiceLife = table.Column<TimeSpan>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Plane", x => x.Id);
+            table.ForeignKey(
+                      name: "FK_Plane_PlaneType_PlaneTypeId",
+                      column: x => x.PlaneTypeId,
+                      principalTable: "PlaneType",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.Cascade);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Departure",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    FlightId = table.Column<int>(nullable: false),
-                    Date = table.Column<DateTime>(nullable: false),
-                    CrewId = table.Column<int>(nullable: false),
-                    PlaneId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Departure", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Departure_Crew_CrewId",
-                        column: x => x.CrewId,
-                        principalTable: "Crew",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Departure_Flight_FlightId",
-                        column: x => x.FlightId,
-                        principalTable: "Flight",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Departure_Plane_PlaneId",
-                        column: x => x.PlaneId,
-                        principalTable: "Plane",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+      migrationBuilder.CreateTable(
+          name: "Departure",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            FlightId = table.Column<int>(nullable: false),
+            Date = table.Column<DateTime>(nullable: false),
+            CrewId = table.Column<int>(nullable: false),
+            PlaneId = table.Column<int>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Departure", x => x.Id);
+            table.ForeignKey(
+                      name: "FK_Departure_Crew_CrewId",
+                      column: x => x.CrewId,
+                      principalTable: "Crew",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.Cascade);
+            table.ForeignKey(
+                      name: "FK_Departure_Flight_FlightId",
+                      column: x => x.FlightId,
+                      principalTable: "Flight",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.Cascade);
+            table.ForeignKey(
+                      name: "FK_Departure_Plane_PlaneId",
+                      column: x => x.PlaneId,
+                      principalTable: "Plane",
+                      principalColumn: "Id",
+                      onDelete: ReferentialAction.Cascade);
+          });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Airhostess_CrewId",
-                table: "Airhostess",
-                column: "CrewId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Airhostess_CrewId",
+          table: "Airhostess",
+          column: "CrewId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Departure_CrewId",
-                table: "Departure",
-                column: "CrewId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Departure_CrewId",
+          table: "Departure",
+          column: "CrewId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Departure_FlightId",
-                table: "Departure",
-                column: "FlightId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Departure_FlightId",
+          table: "Departure",
+          column: "FlightId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Departure_PlaneId",
-                table: "Departure",
-                column: "PlaneId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Departure_PlaneId",
+          table: "Departure",
+          column: "PlaneId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Plane_PlaneTypeId",
-                table: "Plane",
-                column: "PlaneTypeId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Plane_PlaneTypeId",
+          table: "Plane",
+          column: "PlaneTypeId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Ticket_FlightId",
-                table: "Ticket",
-                column: "FlightId");
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Airhostess");
-
-            migrationBuilder.DropTable(
-                name: "Departure");
-
-            migrationBuilder.DropTable(
-                name: "Pilot");
-
-            migrationBuilder.DropTable(
-                name: "Ticket");
-
-            migrationBuilder.DropTable(
-                name: "Crew");
-
-            migrationBuilder.DropTable(
-                name: "Plane");
-
-            migrationBuilder.DropTable(
-                name: "Flight");
-
-            migrationBuilder.DropTable(
-                name: "PlaneType");
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_Ticket_FlightId",
+          table: "Ticket",
+          column: "FlightId");
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "Airhostess");
+
+      migrationBuilder.DropTable(
+          name: "Departure");
+
+      migrationBuilder.DropTable(
+          name: "Pilot");
+
+      migrationBuilder.DropTable(
+          name: "Ticket");
+
+      migrationBuilder.DropTable(
+          name: "Crew");
+
+      migrationBuilder.DropTable(
+          name: "Plane");
+
+      migrationBuilder.DropTable(
+          name: "Flight");
+
+      migrationBuilder.DropTable(
+          name: "PlaneType");
+    }
+  }
 }
