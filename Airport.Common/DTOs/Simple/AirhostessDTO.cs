c1c4d7a6b1c868e149aead6ace2d3e849621010c
paladin4ua <igzhva@gmail.com>
2018-07-28 17:42:40 +0300

    prepared for BsaHw10

--- Airport.Common/DTOs/Simple/AirhostessDTO.cs
+++ Airport.Common/DTOs/Simple/AirhostessDTO.cs
@@ -8,6 +8,6 @@ namespace Airport.Common.DTOs
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime BirthDate { get; set; }
-    public int CrewId { get; set; }
+    public int? CrewId { get; set; }
   }
 }
--- Airport.Common/Validation/AirhostessDTOValidator.cs
+++ Airport.Common/Validation/AirhostessDTOValidator.cs
@@ -9,7 +9,6 @@ namespace Airport.Common.Validation
   {
     public AirhostessDTOValidator()
     {
-      RuleFor(x => x.CrewId).GreaterThan(0).WithMessage("Crew Id should be greater than 0");
       RuleFor(x => x.BirthDate).NotEmpty();
       RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name should not be empty");
       RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name should not be empty");
